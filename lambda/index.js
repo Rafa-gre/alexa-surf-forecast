caccc75548d06ce28c2af46de56225366e36c344
Rafael <rafael@gregorini.com.br>
2024-04-23 14:05:28 -0300

    adjusting typescript config

--- lambda/index.js
+++ lambda/index.js
@@ -4,7 +4,7 @@
  * session persistence, api calls, and more.
  * */
 import { getRequestType, getIntentName, SkillBuilders, DefaultApiClient } from 'ask-sdk';
-import { generateForecastSpeech } from './dist/services/generateForecastSpeech';
+import { generateForecastSpeech } from './dist/services/generateForecastSpeech.js';
 
 
 const LaunchRequestHandler = {
